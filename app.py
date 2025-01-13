9ad3cab2a13db03b84d3fd7a7c3dc8e3846e538c
GUTZ333 <lpsking32@gmail.com>
2025-01-13 17:07:42 -0300

    app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, flash, render_template, redirect, url_for, request
 from sql_api import SqlSERVER
-site = Flask(__name__)
-@site.route("/")
+app = Flask(__name__)
+@app.route("/")
 def home():
     return render_template("index.html")
 
-@site.route("/sign-up")
+@app.route("/sign-up")
 def signUp():
     return render_template("signUp.html")
-@site.route("/sign-up/submit", methods=["POST"])
+@app.route("/sign-up/submit", methods=["POST"])
 def submitSignUp():
     name = request.form.get("name")
     email = request.form.get("email")
@@ -18,10 +18,10 @@ def submitSignUp():
     sql_instance.dataAccountAttributes(name=name, email=email, password=password, phone=phone)
     return sql_instance.SignUp(flash, redirect, url_for, render_template) 
 
-@site.route("/sign-in")
+@app.route("/sign-in")
 def signIn():
     return render_template("signIn.html")
-@site.route("/sign-in/submit", methods=["POST"])
+@app.route("/sign-in/submit", methods=["POST"])
 def submitSignIn():
     email = request.form.get("email")
     password = request.form.get("password")
@@ -30,6 +30,6 @@ def submitSignIn():
     return sql_instance.SignIn(flash, redirect, url_for, render_template)
 
 if __name__ == "__main__":
-    site.secret_key = 'super secret key'
-    site.run()
-    site.config["SESSION_TYPE"] = "filesystem"
+    app.secret_key = 'super secret key'
+    app.run()
+    app.config["SESSION_TYPE"] = "filesystem"
